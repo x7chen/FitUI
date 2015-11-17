b9dc9958d49c761123d64ee003797f0103d0730d
x7chen@126.com <github1512>
2015-11-17 12:07:39 +0800

    backup

--- app/src/main/java/com/x7chen/dev/fitui/BondActivity.java
+++ app/src/main/java/com/x7chen/dev/fitui/BondActivity.java
@@ -1,17 +1,19 @@
 package com.x7chen.dev.fitui;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.internal.view.menu.MenuBuilder;
+import android.support.v7.widget.ActionMenuPresenter;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 
-public class AlarmActivity extends AppCompatActivity {
+public class BondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm);
+        setContentView(R.layout.activity_bond);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_setting);
         toolbar.setNavigationIcon(android.R.drawable.ic_menu_revert);
         setSupportActionBar(toolbar);
@@ -21,12 +23,11 @@ public class AlarmActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        getMenuInflater().inflate(R.menu.menu_setting,menu);
         return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/x7chen/dev/fitui/MainActivity.java
+++ app/src/main/java/com/x7chen/dev/fitui/MainActivity.java
@@ -96,10 +96,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_user_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        } else if (id == R.id.nav_bond) {
+            Intent intent = new Intent(this,BondActivity.class);
+            startActivityForResult(intent, 123);
         } else if (id == R.id.nav_alarm) {
             Intent intent = new Intent(this,AlarmActivity.class);
             startActivityForResult(intent, 123);
